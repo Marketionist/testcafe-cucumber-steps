dce0ef0898994ecc635ea092406f07d1731dfe3d
Marketionist <marketionist@gmail.com>
2019-12-29 23:21:18 +0200

    Add 'I go to URL' and 'the title should be' steps

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+'use strict';
+/* eslint new-cap: 0 */ // --> OFF for Given, When, Then
+
 // #############################################################################
+
+const { Given, When, Then } = require('cucumber');
+const { ClientFunction } = require('testcafe');
+
+// #### When steps #########################################################
+
+When(/^I go to URL "([^"]*)"$/, async function (t, [url]) {
+    await t.navigateTo(url);
+});
+
+// #### Then steps #########################################################
+const getTitle = ClientFunction(() => {
+    return document.title;
+});
+
+Then(/the title should be "([^"]*)"$/, async function (t, [text]) {
+    await t.expect(getTitle()).eql(text);
+});
