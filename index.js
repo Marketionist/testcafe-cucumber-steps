1cbf601d540bee39e7b2f7dd1561ce6760c05fb9
Marketionist <marketionist@gmail.com>
2020-01-01 15:41:46 +0200

    Migrate steps from Regular Expressions to Cucumber Expressions

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { ClientFunction } = require('testcafe');
 
 // #### When steps #############################################################
 
-When(/^I go to URL "([^"]*)"$/, async function (t, [url]) {
+When('I go to URL {string}', async function (t, [url]) {
     await t.navigateTo(url);
 });
 
@@ -18,10 +18,10 @@ const getTitle = ClientFunction(() => {
     return document.title;
 });
 
-Then(/the title should be "([^"]*)"$/, async function (t, [text]) {
+Then('the title should be {string}', async function (t, [text]) {
     await t.expect(getTitle()).eql(text);
 });
 
-Then(/the title should contain "([^"]*)"$/, async function (t, [text]) {
+Then('the title should contain {string}', async function (t, [text]) {
     await t.expect(getTitle()).contains(text);
 });
