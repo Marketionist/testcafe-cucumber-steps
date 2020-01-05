f3a003b9766efb7efc2bdd3f029e68315976c9b3
Marketionist <marketionist@gmail.com>
2020-01-05 14:18:42 +0200

    Add PO parsing and 'I go to page' steps

--- index.js
+++ index.js
@@ -5,11 +5,39 @@
 
 const { Given, When, Then } = require('cucumber');
 const { ClientFunction } = require('testcafe');
+const pageObjectsFolderPath = process.env.PO_FOLDER_PATH || 'tests/page-model';
+const path = require('path');
+const fs = require('fs');
+
+const fullPageObjectsFolderPath = __dirname.includes('node_modules') ?
+    path.join(__dirname, '../..', pageObjectsFolderPath) :
+    path.join(__dirname, pageObjectsFolderPath);
+
+// Require all Page Object files in directory
+const pageObjects = {};
+
+fs.readdirSync(fullPageObjectsFolderPath).filter(
+    (value) => value.includes('.js')
+).map((file) => {
+    const fileName = path.basename(file, '.js');
+
+    pageObjects[fileName] = require(path.join(fullPageObjectsFolderPath, file));
+});
+
+console.log('Page Objects found:', pageObjects);
 
 // #### When steps #############################################################
 
 When('I go to URL {string}', async function (t, [url]) {
     await t.navigateTo(url);
+});
+
+When('I go to {string}.{string}', async function (t, [page, element]) {
+    await t.navigateTo(pageObjects[page][element]);
+});
+
+When('I go to {word} from {word} page', async function (t, [element, page]) {
+    await t.navigateTo(pageObjects[page][element]);
 });
 
 // #### Then steps #############################################################
