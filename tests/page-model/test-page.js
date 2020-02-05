84d1d66456a3eb307ce508b193dea3bf782f5506
Marketionist <marketionist@gmail.com>
2020-02-04 20:42:25 -0500

    Simplify Page Objects

--- tests/page-model/test-page.js
+++ tests/page-model/test-page.js
@@ -1,21 +1,21 @@
 'use strict';
 
-module.exports = (function () {
+// #############################################################################
 
-    let testPage = {
+let testPage = {
 
-        pageTest1: 'http://localhost:8001/test1.html',
-        textError: `//*[ancestor::*[@class="todo-container" and 
-            descendant::*[text()="New"]] and @type="checkbox"]`,
-        linkTest2Page: '#link-test2-page',
-        titleTest1: 'h1',
-        blockTextTest: '#text-test',
-        txtTest1: 'Test 1 sample',
-        txtTest2: 'Test2',
-        linkInvisibleTest2Page: '#link-invisible-test2-page'
+    pageTest1: 'http://localhost:8001/test1.html',
+    textErrorXPath: `//*[ancestor::*[@class="todo-container" and 
+        descendant::*[text()="New"]] and @type="checkbox"]`,
+    linkTest2Page: '#link-test2-page',
+    linkTest2PageXPath: '//*[@id="link-test2-page"]',
+    titleTest1: 'h1',
+    blockTextTest: '#text-test',
+    txtTest1: 'Test 1 sample',
+    txtTest2: 'Test2',
+    linkInvisibleTest2Page: '#link-invisible-test2-page',
+    linkInvisibleTest2PageXPath: '//*[@id="link-invisible-test2-page"]'
 
-    };
+};
 
-    return testPage;
-
-})();
+module.exports = testPage;
--- tests/page-model/test2-page.js
+++ tests/page-model/test2-page.js
@@ -1,23 +1,23 @@
 'use strict';
 
-module.exports = (function () {
+// #############################################################################
 
-    let test2Page = {
+let test2Page = {
 
-        pageTest2: 'http://localhost:8001/test2.html',
-        textGold: 'Gold',
-        textIndigo: 'Indigo',
-        dropdownColors: '#dropdown-colors',
-        blockDropdownColor: '#block-dropdown-color',
-        inputColors: '#input-colors',
-        blockInputColor: '#block-input-color',
-        updateText: function () {
-            document.getElementById('text-test').innerHTML = 'Text to test ' +
-                'script execution';
-        }
+    protocol: 'http://',
+    textGold: 'Gold',
+    textIndigo: 'Indigo',
+    dropdownColors: '#dropdown-colors',
+    blockDropdownColor: '#block-dropdown-color',
+    inputColors: '#input-colors',
+    blockInputColor: '#block-input-color',
+    updateText: function () {
+        document.getElementById('text-test').innerHTML = 'Text to test ' +
+            'script execution';
+    }
 
-    };
+};
 
-    return test2Page;
+test2Page.pageTest2 = `${test2Page.protocol}localhost:8001/test2.html`;
 
-})();
+module.exports = test2Page;
