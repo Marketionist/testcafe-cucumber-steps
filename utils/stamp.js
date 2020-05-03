8f2417c491d18c5563af2a93e1efed8164e407db
Marketionist <marketionist@gmail.com>
2020-05-03 19:16:37 -0400

    Add getTimestamp and resetTimestamp

--- utils/stamp.js
+++ utils/stamp.js
@@ -3,18 +3,23 @@
 // #############################################################################
 
 let stamp = {
-    timestamp: '',
-    setTimestamp: function () {
-        this.timestamp = new Date().getTime().toString();
-        process.env.TIMESTAMP = this.timestamp;
+    _timestamp: '',
+    resetTimestamp: function () {
+        this._timestamp = new Date().getTime().toString();
+        process.env.TIMESTAMP = this._timestamp;
 
         console.log(`process.env.TIMESTAMP: ${process.env.TIMESTAMP}`);
+
+        return process.env.TIMESTAMP;
+    },
+    getTimestamp: function () {
+        process.env.TIMESTAMP = this._timestamp.length > 0 ? this._timestamp : this.resetTimestamp()
 
         return process.env.TIMESTAMP;
     }
 };
 
-stamp.setTimestamp();
+stamp.getTimestamp();
 
 console.log(`process.env.TIMESTAMP: ${process.env.TIMESTAMP}`);
 
