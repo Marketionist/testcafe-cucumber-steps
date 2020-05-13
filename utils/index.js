e9a062a2a1998d44ec74a4a52ab6593f21f83962
Marketionist <marketionist@gmail.com>
2020-05-13 00:05:38 -0400

    Include create-request in utils index

--- utils/index.js
+++ utils/index.js
@@ -5,9 +5,11 @@
 const SelectorXPath = require('./selector-xpath.js');
 const readDirectories = require('./read-directories.js');
 const stamp = require('./stamp.js');
+const createRequest = require('./create-request.js');
 
 module.exports = {
     SelectorXPath: SelectorXPath,
     readDirectories: readDirectories,
-    stamp: stamp
+    stamp: stamp,
+    createRequest: createRequest
 };
