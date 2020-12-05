6525835e08c3233951d9aaa9439d20cac6686b78
Marketionist <marketionist@gmail.com>
2020-12-04 21:48:21 -0500

    Add texts for getTitle and getCurrentPageUrl

--- utils/errors.js
+++ utils/errors.js
@@ -5,5 +5,7 @@ module.exports = {
         'defined in Page Object:',
     ELEMENT_NOT_PRESENT: 'expected element to be present:',
     ELEMENT_PRESENT: 'expected element not to be present:',
-    ATTRIBUTE_NOT_INCLUDES: 'expected element\'s attribute to include value:'
+    ATTRIBUTE_NOT_INCLUDES: 'expected element\'s attribute to include value:',
+    NO_TITLE: 'Can not get title of the current page',
+    NO_URL: 'Can not get URL of the current page'
 };
