9f0b8f9abb2b731a2fd075e82cc7a9f829f68b1c
Marketionist <marketionist@gmail.com>
2023-11-23 00:19:38 -0500

    Separate and expose getting page objects

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 // #############################################################################
 
 const { ClientFunction, Selector } = require('testcafe');
-const path = require('path');
-const { readDirectories, createRequest } = require('js-automation-tools');
+const { createRequest } = require('js-automation-tools');
 const SelectorXPath = require('./utils/selector-xpath.js');
+const requirePageObjects = require('./utils/get-page-objects.js');
 const errors = require('./utils/errors.js');
 
 let Given;
@@ -26,51 +26,8 @@ try {
 
 const spacesToIndent = 4;
 
-const isCalledExternally = __dirname.includes('node_modules');
-
-const pageObjectsFolderPathes = 'PO_FOLDER_PATH' in process.env ?
-    process.env.PO_FOLDER_PATH.replace(/\s+/g, '').split(',') :
-    [path.join('tests', 'page-model')];
-
-const fullPageObjectsFolderPathes = isCalledExternally ?
-    pageObjectsFolderPathes.map((pageObjectsFolderPath) => {
-        return path.join(__dirname, '..', '..', pageObjectsFolderPath)
-    }) :
-    pageObjectsFolderPathes.map((pageObjectsFolderPath) => {
-        return path.join(__dirname, pageObjectsFolderPath)
-    });
-
 // Require all Page Object files in directory
-let pageObjects = {};
-
-/**
- * Requires Page Object files
- * @returns {Array} allRequiredPageObjects
- */
-async function requirePageObjects () {
-    const allPageObjectFiles = await readDirectories(
-        fullPageObjectsFolderPathes);
-    const allRequiredPageObjects = allPageObjectFiles.filter(
-        (value) => {
-            return value.includes('.js');
-        }
-    ).map((file) => {
-        const fileName = path.basename(file, '.js');
-
-        pageObjects[fileName] = require(file);
-
-        return file;
-    });
-
-    console.log(
-        '\nPage Objects from PO_FOLDER_PATH:',
-        `\n${JSON.stringify(pageObjects, null, spacesToIndent)}\n\n`
-    );
-
-    return allRequiredPageObjects;
-}
-
-requirePageObjects();
+let pageObjects = requirePageObjects();
 
 /**
  * Checks if locator is XPath
---
+++ utils/get-page-objects.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// #############################################################################
+
+const path = require('path');
+const { readDirectories } = require('js-automation-tools');
+
+const isCalledExternally = __dirname.includes('node_modules');
+
+const pageObjectsFolderPathes = 'PO_FOLDER_PATH' in process.env ?
+    process.env.PO_FOLDER_PATH.replace(/\s+/g, '').split(',') :
+    [path.join('tests', 'page-model')];
+
+const fullPageObjectsFolderPathes = isCalledExternally ?
+    pageObjectsFolderPathes.map((pageObjectsFolderPath) => {
+        return path.join(__dirname, '..', '..', pageObjectsFolderPath)
+    }) :
+    pageObjectsFolderPathes.map((pageObjectsFolderPath) => {
+        return path.join(__dirname, pageObjectsFolderPath)
+    });
+
+// Require all Page Object files in directory
+let pageObjects = {};
+
+/**
+ * Requires Page Object files
+ * @returns {Array} allRequiredPageObjects
+ */
+async function requirePageObjects () {
+    const allPageObjectFiles = await readDirectories(
+        fullPageObjectsFolderPathes);
+    const allRequiredPageObjects = allPageObjectFiles.filter(
+        (value) => {
+            return value.includes('.js');
+        }
+    ).map((file) => {
+        const fileName = path.basename(file, '.js');
+
+        pageObjects[fileName] = require(file);
+
+        return file;
+    });
+
+    console.log(
+        '\nPage Objects from PO_FOLDER_PATH:',
+        `\n${JSON.stringify(pageObjects, null, spacesToIndent)}\n\n`
+    );
+
+    return allRequiredPageObjects;
+}
+
+module.exports = requirePageObjects;
